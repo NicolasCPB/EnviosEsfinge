ff1fab687eaead669dc040c64f4445c4cab37902
Edilson8052 <edilson.oliveira@tcesc.tc.br>
2024-03-19 10:17:42 -0300

    Melhorias caracteres

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,11 @@
 import simplejson
+# -*- coding: utf-8 -*-
 
 def montarLogEnvioRemessa(msg, msgErro):
 
     if msgErro != "":
         msg += str(msgErro)
 
-    with open("logEnvio.txt", 'a') as logger:
+    with open("logEnvio.txt", 'a', encoding='utf-8') as logger:
         simplejson.dump(msg, logger, indent=2)
+        logger.write("\n")
