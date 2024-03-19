b98fd6f5bb663f2f95b337eaef8760d885f44d4d
NICOLAS CARVALHO PINTO BECKER <8047@tcesc.tc.br>
2024-03-19 09:51:17 -0300

    Remoção dos logs de testes

--- enviosComParcial/tributario/enviar.py
+++ enviosComParcial/tributario/enviar.py
@@ -105,17 +105,13 @@ def enviaMultiplosJsons(quantidadeArquivos):
                 caminho_diretorio = os.path.join("C:", "arquivos",)
                 caminho_arquivo = os.path.join(caminho_diretorio, nomeArquivo)
                 with open(caminho_arquivo, "r", encoding="utf-8") as arquivo:
-                    montarLogEnvioRemessa(f'Lendo JSON para envio. {getDataAtualString()}', '')
                     dados = simplejson.load(arquivo)
-                    montarLogEnvioRemessa(f'Finalizada leitura do JSON para envio. {getDataAtualString()}', '')
             except UnicodeDecodeError as e:
                 print(f"Erro de decodificação: {e}")
             except json.JSONDecodeError as e:
                 print(f"Erro de decodificação JSON: {e}")
             
-            montarLogEnvioRemessa(f'Enviando remessa para o e-Sfinge Online. {getDataAtualString()}', "")
             response = requests.post(url, headers=headers, params=params, json=dados)
-            montarLogEnvioRemessa(f'Envio realizado com sucesso. {getDataAtualString()}', "")
             response.raise_for_status()
 
             #Limpa os dados da memória
